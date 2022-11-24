419cafa4ac71978a79ccbe871cd2c97fb173dfa7
Dragos-Paul Pop <froind@gmail.com>
2022-11-24 09:55:51 +0200

    auth

---
+++ lab6/addForm.php
@@ -0,0 +1,6 @@
+<form action="add.php" method="post">
+  <input type="text" name="title">
+  <button class="waves-effect waves-light green btn">
+    <i class="material-icons white-text">add</i>
+  </button>
+</form>
---
+++ lab6/delete.php
@@ -0,0 +1,27 @@
+<?php
+
+  require_once('./secure.php');
+
+  if (isset($loggedIn) && $loggedIn) {    
+    $userId = isset($_POST['userId']) ? $_POST['userId'] : false;
+    $id = isset($_POST['id']) ? $_POST['id'] : false;
+
+    if ($userId !== false && $id !== false) {
+      require_once('./readDb.php');
+
+      $newTodos = [];
+      foreach($toDos as $todo) {
+        if (!($todo['userId'] == $userId && $todo['id'] == $id)) {
+          $newTodos[] = $todo;
+        }
+      }
+
+      $stringData = json_encode($newTodos);
+      file_put_contents($dbFile, $stringData);
+    }
+
+    header('Location: index.php');
+  }else {
+    header('Location: login.php');
+  }
+?>
